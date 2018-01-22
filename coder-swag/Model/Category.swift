b15c574fd4d5968d37c60bc5d200afe1c767e3cb
Barış Cesim <berva@Bars-MacBook-Pro.local>
2018-01-22 22:46:54 +0300

    Starting Pharse 2

--- coder-swag/Model/Category.swift
+++ coder-swag/Model/Category.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+struct Category{
+    // Private ( Set) ile, Buradaki değişkenleri artık başka bir class'a atayamazsın ama görebilirsin.
+    private(set) public var title: String!
+    private(set) public var imageName: String!
+    
+    init(title: String, imageName: String){
+        self.title = title
+        self.imageName = imageName
+        
+    }
+}
--- coder-swag/Services/DataService.swift
+++ coder-swag/Services/DataService.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+class DataService{
+    // Static on memory de sadece bir kopya üretilecek
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    func getCategories() -> [Category]{
+        return categories
+    }
+}
