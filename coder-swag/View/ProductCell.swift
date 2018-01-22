99ea81a4476286a1d5865574a1b3f28c7fd18e5b
Barış Cesim <berva@Bars-MacBook-Pro.local>
2018-01-22 23:48:46 +0300

    Update Second Lesson

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,21 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Barış Cesim on 22.01.2018.
+//  Copyright © 2018 bcesim. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
